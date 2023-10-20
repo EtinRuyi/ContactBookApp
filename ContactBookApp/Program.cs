c3ded95f9d69730aea641ac5bbd9ed25cb3a6ee2
EtinRuyi <omoruyi.idowu@yahoo.com>
2023-10-20 05:05:01 +0100

    Cinfigure JWT

--- ContactBookApp/Program.cs
+++ ContactBookApp/Program.cs
@@ -1,6 +1,8 @@
 
 using ContactBookApp.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Security.Cryptography.Xml;
 
 namespace ContactBookApp
 {
@@ -16,7 +18,34 @@ namespace ContactBookApp
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("V1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Authorization", Version = "v1" });
+                config.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme 
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Insert Token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "Jwt",
+                    Scheme = "Bearer"
+                });
+
+                config.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityRequirement
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
 
             var app = builder.Build();
 
